c538eecce9d4ef3017dce448bf8e9fbce1f03ff7
AlreadyMasos <marura3011@gmail.com>
2022-07-10 12:14:02 +0300

    minor changes

--- first_task.py
+++ first_task.py
@@ -3,9 +3,12 @@ def task(array):
         if int(k) == 0:
             return i
 
+def task1(array):
+    return array.find('0')
+
+
 print(task("111111111111000000000000000"))
-
-
+print(task1("111111111111000000000000000"))
 
 
 
